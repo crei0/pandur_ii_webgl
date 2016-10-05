ec67f1a7ba7e2ded35448c3e0172cbc2775bad30
Andre Guedes <guedes.andre@gmail.com>
2016-10-05 01:08:46 +0100

    initialize repository files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pandur_ii_webgl
-Pandur II - WebGL
+# Pandur II - WebGL
+
+WebGL experiment where you are able to rotate and activate a 3D wireframe visualization of the [PandurII](https://en.wikipedia.org/wiki/Pandur_II) vehicle.
+
+This experiment uses the JavaScript 3d engine [Three.JS](https://github.com/mrdoob/three.js/).
---
+++ sombra.js
@@ -0,0 +1,54 @@
+// Converted from: sombra.obj
+//  vertices: 4
+//  faces: 1
+//  normals: 1
+//  colors: 0
+//  uvs: 4
+//  materials: 1
+//  edges: 0
+//
+//  Generated with OBJ -> Three.js converter
+//  http://github.com/alteredq/three.js/blob/master/utils/exporters/convert_obj_three.py
+
+
+var model = {
+
+    "version" : 2,
+
+    "scale" : 1.000000,
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "04___Default",
+	"colorAmbient" : [0.5882, 0.5882, 0.5882],
+	"colorDiffuse" : [0.5882, 0.5882, 0.5882],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapAmbient" : "sombra.jpg",
+	"mapDiffuse" : "sombra.jpg",
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparency" : 0.0
+	}],
+
+    "vertices": [78.286400,0.000000,-182.418600,-78.286400,0.000000,-182.418600,-78.286400,0.000000,182.418600,78.286400,0.000000,182.418600],
+
+    "morphTargets": [],
+
+    "morphColors": [],
+
+    "normals": [0,1,-0],
+
+    "colors": [],
+
+    "uvs": [[0.9995,0.0005,0.0005,0.0005,0.0005,0.9995,0.9995,0.9995]],
+
+    "faces": [43,0,1,2,3,0,0,1,2,3,0,0,0,0],
+
+    "edges" : []
+
+};
+
+postMessage( model );
+close();
